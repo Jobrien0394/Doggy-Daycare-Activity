b193b2f338b0d16f865423d25b9a63e333709d78
Jobrien0394 <84590243+Jobrien0394@users.noreply.github.com>
2021-09-03 13:15:14 -0400

    Couldn't get it to display "No Dogs Yet!"

--- code.js
+++ code.js
@@ -1,85 +1,106 @@
-// Follow the Instructions on my.kenzie.academy for this assignment.
-// Those instructions will give you details on each step.
-
-// STEP ONE - Create your Data Model.
-let dataModel = null;  // Assign your data model here, instead of null. This should be an array of "dog" objects.
-
-
-// When this function is run, it is meant to use the user input to build
-// a dog object, and add the dog object to the data model array.
-function onSubmitDog (event) {
-  event.preventDefault();
-
-  let nameInput = document.querySelector("#name_input");  // We provide a CSS selector, as a string, to identify which HTML element we want querySelector to find for us.
-  let breedInput = document.querySelector("#breed_input");
-  let ageInput = document.querySelector("#age_input");
-  let treatsCheckbox = document.querySelector("#treats_input");
-
-  let name = nameInput.value;
-  let breed = breedInput.value;
-  let age = ageInput.value;
-  let likesTreats = treatsCheckbox.checked;
-  
-  if (name === "" || breed === "" || age === "") {  // If any of these text boxes are empty...
-    alert("Please fill out all of the fields!");
-    return;  // Exit the function early if the above condition is true.
+/* CREATE TABLE DOGTABLE name breed age likesTreats ;
+ */
+dataModel = [
+  {
+    name: "Lilly",
+    breed: "Blue Heeler/Beagle Mix",
+    age: 2,
+    likesTreats: true
   }
+  ,{
+    name: "Teddy",
+    breed: "Chihuahua/Shih Tzu Mix",
+    age: 5,
+    likesTreats: true
+  }
+  ,{
+    name: "Rocky",
+    breed: "Corgie/Australian Shepherd Mix",
+    age: 1,
+    likesTreats: true
+  }
+]
 
-  // STEP TWO - Create a "dog" variable. What piece of data will we assign
-  // to this variable? A new "dog" object, containing the values from above:
-  // name, breed, age, likeTreats. Add this object to your data model array.
-  // How can you insert this dog object into the dogs array?
+/* INSERT INTO DOGTABLE DOG;
+   step 2
+ */
+let athletic_dog = {};     // = new Object();
+athletic_dog.name         = "AirBud";
+athletic_dog.breed        = "Golden Retriever";
+athletic_dog.age          = "3";
+athletic_dog.likesTreats = true;
+dataModel.push(athletic_dog);
 
-  // YOUR CODE HERE
-
-
-  renderDogList(); // Now that we have added a new dog to the data model,
-                   // we should render the dog list on the page again.
-  
-  // The following lines reset the form, so that it is ready for information
-  // on a new dog:
-  nameInput.value = "";
-  breedInput.value = "";
-  ageInput.value = "";
-  treatsCheckbox.checked = false;
-}
-
-
-// This function is run, it is meant to keep the dog list which the user
-// sees on the page in sync with the data model containing all of our 
-// dog objects.
-function renderDogList() {
-  let list = document.querySelector("#dog_list");
-  list.innerHTML = "";  // First, CLEAR the whole list.
-
-  // STEP THREE - Render the dog list from scratch. See "Step Three"
-  // instructions.
-  // If there are no dogs, then render "No Dogs!" Otherwise, render all 
-  // of the dogs in your data model.
-  // Remember to copy the "Send Home" button code into your loop. This
-  // code is in the instructions.
-
-  // YOUR CODE HERE
-}
-
-
-// The function below is already completed for you. It removes a given 
-// dog from the data model. It finds the index of the dog, and then uses
-// that index to splice (cut) it out of the array. Then it re-renders
-// the dog list, so that it no longer displays on the page.
+/* DELETE FROM DOGTABLE WHERE DOG=dog;
+ */
 function removeDog(dog) {
   let dogIndex = dataModel.indexOf(dog);
   dataModel.splice(dogIndex, 1);
-
   renderDogList();
 }
 
 
-// We need to tell the Submit button on the page what to do:
-// Run the onSubmitDog function when the button is clicked.
+/* INSERT INTO DOGTABLE DOG;
+   use the user input to build a dog object, 
+*/
+function onSubmitDog (event) {
+  event.preventDefault(); // If the fields are incomplete, do not clear them
+  // A CSS selector as a string to identify which HTML element we want
+  let nameInput     =   document.querySelector("#name_input");
+  let breedInput    =   document.querySelector("#breed_input");
+  let ageInput    =    document.querySelector("#age_input");
+  let treatsCheckbox = document.querySelector("#treats_input");
+  let name      =      nameInput.value;
+  let breed = breedInput.value;
+  let age = ageInput.value;
+  let likesTreats = treatsCheckbox.checked;
+  if (name === "" || breed === "" || age === "") {
+    alert("Please fill out all of the fields!");
+    return 0;  // Exit the function early if the above condition is true.
+  }
+  dog ={};     // = new Object();
+  dog.name = name;
+  dog.breed = breed;
+  dog.age = age;
+  dog.likesTreats = likesTreats;
+  dataModel.push(dog);  // store it
+  
+  renderDogList();  // we should render the dog list on the page again.
+  nameInput.value        = "";   // clear input forms
+  breedInput.value       = "";
+  ageInput.value         = "";
+  treatsCheckbox.checked = false;
+}
+
 let button = document.querySelector("#submit_button");
 button.addEventListener("click", onSubmitDog);
 
-// This function call will take place when the page loads, in order
-// to render the dog list for the very first time.
+/*
+ */
+function renderDogList() {
+  let ulTag = document.querySelector("#dog_list");
+  ulTag.innerHTML = "";    // Delete everything inside ul 
+                          // including elements  
+  for(let d of dataModel){
+    lambda = document.createElement("li"); // create a list item
+    lambda.innerHTML = `${d.name}! A ${d.age} year old ${d.breed}${(d.likesTreats?" who likes treats":"")}. `;
+        // assign text of that element
+    
+    button = document.createElement("button");  //code dealing with the button
+    button.innerHTML="Send Home";
+    button.addEventListener("click", function(e){removeDog(d);})
+    lambda.append(button);
+    
+    ulTag.append(lambda); //append li to ul
+  }
+}
+/* RENDER "NO DOGS!" WHEN THERES NO DOGS ON LISTS */
+
+// if the array is empty, return "NO DOGS YET! 🙂"//
+
+if (dataModel === undefined || dataModel == 0) {
+  datamodel.innerHTML = "NO DOGS YET 🙂";
+} 
+
+// The page loads, to render the dog list for the very first time.
 renderDogList();
